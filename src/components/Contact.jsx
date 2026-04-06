a8fd1d43156bdd929238aa55d1b41048290016e0
ellen0825 <ellenmartinelli0825@outlook.com>
2026-04-06 20:41:52 +0300

    2026-4-6-6

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ export default function Contact() {
     setStatus("Отправка...");
 
     try {
-      const response = await fetch("https://formspree.io/f/YOUR_FORM_ID", { 
+      const response = await fetch("https://formspree.io/f/mqegwvzz", { 
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
